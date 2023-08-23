3284f7750bff944432cefc79d646574bd85283a6
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-23 17:40:24 +0200

    add prompt, if else and toFixed

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const distance = Number(prompt("Inserisci il numero di chilometri:"));
+
+const age = Number(prompt("Inserisci la tua età:"));
+
+const basePrice = distance * 0.21;
+
+let discount;
+
+if (age <= 18){
+   discount = basePrice * 0.8;
+} else if (age >= 65){
+   discount = basePrice * 0.6;
+}
+
+let discountRounded = discount.toFixed(2);
+
+document.getElementById("price").innerHTML = discountRounded;
