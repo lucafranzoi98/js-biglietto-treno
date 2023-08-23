756e55afe1b04a813b86b2bdd6879f49eafc946a
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-23 22:55:09 +0200

    debugging

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,12 @@ const basePrice = distance * 0.21;
 
 let discount;
 
-if (age <= 18){
+if (age < 18){
    discount = basePrice * 0.8;
-} else if (age >= 65){
+} else if (age > 65){
    discount = basePrice * 0.6;
+} else{
+   discount = basePrice;
 }
 
 let discountRounded = discount.toFixed(2);
